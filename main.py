d1183318dad4a08ce32ca63644956dfa8375a8bb
chenigovtsev2001mail.ru <chernigovtsev2001@mail.ru>
2026-06-04 22:53:41 +0300

    small refactoring

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ process_pool = ProcessPoolExecutor(max_workers=4)
 async def remove_background(file: UploadFile = File(...)):
     file_bytes = await file.read()
 
-    loop = asyncio.get_running_loop()
-    output_bytes = await loop.run_in_executor(process_pool, remove_background_sync, file_bytes)
+    # loop = asyncio.get_running_loop()
+    # output_bytes = await loop.run_in_executor(process_pool, remove_background_sync, file_bytes)
+    output_bytes = remove_background_sync(file_bytes)
 
     return StreamingResponse(BytesIO(output_bytes), media_type="image/png")
 
@@ -32,6 +33,15 @@ async def get_wb_image(url: str = Query(..., description="URL товара Wildb
 
     processed_bytes = remove_background_sync(png_bytes)
     return StreamingResponse(BytesIO(processed_bytes), media_type="image/png")
+
+
+@app.get("/")
+async def hello():
+    return {"hello": "world"}
+
+@app.get("/hello")
+async def hello1():
+    return {"hello": "world1"}
 
 
 if __name__ == "__main__":
